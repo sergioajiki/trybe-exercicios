cc4f90aac6de2f9b96f42bb1b5aa93d73dd1edc1
Sergio <sergioajk@hotmail.com>
2022-10-20 22:00:01 -0300

    exercicio7

---
+++ fundamentos/introducao-javascript/exedia1/exercicio7.js
@@ -0,0 +1,10 @@
+const a = 10;
+const b = 10;
+
+if (a == b) {
+    console.log("'a' é igual 'b'");
+}   else if (a > b) {
+    console.log("'a' é maior que 'b'");
+}   else if (a < b) {
+    console.log("'b' é maior que 'a'");
+}
