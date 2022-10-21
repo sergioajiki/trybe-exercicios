332a3d2ed42c8e8ebd863b84e5c592c91997215a
Sergio <sergioajk@hotmail.com>
2022-10-21 15:03:34 -0300

    correção

--- fundamentos/introducao-javascript/exercicio2.js
+++
@@ -1,6 +0,0 @@
-let numbers =  [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let totalSoma = 0;
-    for (let index = 0; index < numbers.length; index++) {
-        totalSoma = totalSoma + numbers[index]
-    }
-    console.log(totalSoma);
