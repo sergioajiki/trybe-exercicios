26b13623a2a154e3202791fa474b67588b33175d
Sergio <sergioajk@hotmail.com>
2022-10-25 20:24:22 -0300

    exercicio for/in e for/of

---
+++ fundamentos/introducao-javascript/exedia4/exercicio2.js
@@ -0,0 +1,24 @@
+// Usando o objeto abaixo, utilize for/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+  };
+
+  for (let nome in names) {
+    console.log('Olá, '+ names[nome]);
+  }
+
+//   Usando o objeto abaixo, utilize for/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+};
+
+for (let i in car) {
+    console.log(i + ': ' + car[i]);
+
+}
