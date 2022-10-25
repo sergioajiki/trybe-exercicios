3dd161f31863feb1a579fe4b607427c8581e87fa
Sergio <sergioajk@hotmail.com>
2022-10-25 20:41:24 -0300

    exercicio function

---
+++ fundamentos/introducao-javascript/exedia4/exercicio3.js
@@ -0,0 +1,31 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+function soma(a, b) {
+    return a + b
+}
+
+function subtracao(a, b) {
+    return a - b
+}
+
+function multiplicacao(a, b) {
+    return a * b
+}
+
+function divisao(a, b) {
+    return a / b
+}
+
+function modulo(a, b) {
+    return a % b
+}
+
+console.log(modulo(10, 3))
+
+// Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
