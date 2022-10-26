05e7413bf4b8f8da1a0bc6a902ba1cc8fe10a7be
Sergio <sergioajk@hotmail.com>
2022-10-26 00:03:46 -0300

    exercicio indice maior numero

--- fundamentos/introducao-javascript/exedia4/exercicio4.js
+++ fundamentos/introducao-javascript/exedia4/exercicio4.js
@@ -16,3 +16,28 @@ console.log(verificaPalindromo('radar'));
   
 //   console.log(verificaPalindrome('arara')); //true
 //   console.log(verificaPalindrome('desenvolvimento')); //false
+
+
+
+
+
+// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+// Array de teste: [2, 3, 6, 7, 10, 1];.
+
+// Valor esperado no retorno da função: 4.
+
+
+function indexMaiorValor(numeros) {
+    let indexMaior = 0;
+    for (let index in numeros) {
+        if (numeros[indexMaior] < numeros[index]) {
+            indexMaior = index;
+        }
+    }
+    return indexMaior ; 
+}
+   
+
+console.log(indexMaiorValor([2, 3, 6, 7, 10, 1, 20]))
+
