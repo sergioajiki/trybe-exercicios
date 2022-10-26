0aad641129d6efb315f88bb11897bcc6ba4524a8
Sergio <sergioajk@hotmail.com>
2022-10-26 01:19:26 -0300

    somatoria de N

--- fundamentos/introducao-javascript/exedia4/exercicio4.js
+++ fundamentos/introducao-javascript/exedia4/exercicio4.js
@@ -58,3 +58,16 @@ function indexMenorValor(num) {
 
 
 }console.log(indexMenorValor([2, 4, 6, 7, -10, 0, -30]));
+
+
+// Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+
+function somatoria(numeroN) {
+    let soma = 0
+    for (let index = numeroN; index > 0; index -= 1) {
+        soma = soma + index;
+        
+    }
+    return soma
+}
+console.log(somatoria(6));
